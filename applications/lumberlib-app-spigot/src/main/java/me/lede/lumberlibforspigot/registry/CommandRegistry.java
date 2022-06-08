ca0f9734e29a85fc0b25b74661c33dc6b075f417
Lede <lede.7716@gmail.com>
2022-06-09 02:34:24 +0900

    Add CommandMap null checker

--- applications/lumberlib-app-spigot/src/main/java/me/lede/lumberlibforspigot/registry/CommandRegistry.java
+++ applications/lumberlib-app-spigot/src/main/java/me/lede/lumberlibforspigot/registry/CommandRegistry.java
@@ -4,23 +4,30 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.CommandMap;
 import org.bukkit.command.defaults.BukkitCommand;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.Field;
 
 public class CommandRegistry {
 
     public static boolean register(@NotNull BukkitCommand command) {
+        CommandMap commandMap = getCommandMap();
+        if (commandMap == null) {
+            throw new NullPointerException("Cannot find CommandMap.");
+        }
+        return commandMap.register(command.getName(), command);
+    }
+
+    @Nullable
+    public static CommandMap getCommandMap() {
         try {
             final Field bukkitCommandMap = Bukkit.getServer().getClass().getDeclaredField("commandMap");
-
             bukkitCommandMap.setAccessible(true);
-            CommandMap commandMap = (CommandMap) bukkitCommandMap.get(Bukkit.getServer());
-
-            return commandMap.register(command.getName(), command);
+            return (CommandMap) bukkitCommandMap.get(Bukkit.getServer());
         } catch(Exception e) {
             e.printStackTrace();
         }
-        return false;
+        return null;
     }
 
 }
