2b945d414e063c93d6a3c9ef1291f353aa42137e
Lede <lede.7716@gmail.com>
2022-06-08 23:21:43 +0900

    Add hex color util

---
+++ applications/lumberlib-app-proxy/src/main/java/me/lede/lumberlibforproxy/color/Color.java
@@ -0,0 +1,175 @@
+package me.lede.lumberlibforproxy.color;
+
+import com.google.common.collect.Lists;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.List;
+
+public class Color {
+
+    @NotNull
+    public static List<Component> colored(@Nullable String... messages) {
+        if (messages == null)
+            return Lists.newArrayList();
+
+        List<Component> colored = Lists.newArrayListWithCapacity(messages.length);
+        for (String message : messages) {
+            colored.add(colored(message));
+        }
+        return colored;
+    }
+
+    /**
+     * Add hex color to String[]
+     * @param messages String[] with hex code
+     * @return Colored String List
+     */
+    @NotNull
+    public static List<Component> colored(@Nullable TextComponent... messages) {
+        if (messages == null)
+            return Lists.newArrayList();
+        List<Component> colored = Lists.newArrayListWithCapacity(messages.length);
+        for (TextComponent message : messages) {
+            colored.add(colored(message));
+        }
+        return colored;
+    }
+
+    @NotNull
+    public static List<Component> coloredString(@Nullable Collection<String> messages) {
+        if (messages == null)
+            return Lists.newArrayList();
+
+        List<Component> colored = Lists.newArrayListWithCapacity(messages.size());
+        for (String message : messages) {
+            colored.add(colored(message));
+        }
+        return colored;
+    }
+
+    /**
+     * Add hex color to String Collection
+     * @param messages String Collection with hex code
+     * @return Colored String List
+     */
+    @NotNull
+    public static List<Component> colored(@Nullable Collection<TextComponent> messages) {
+        if (messages == null)
+            return Lists.newArrayList();
+
+        List<Component> colored = Lists.newArrayListWithCapacity(messages.size());
+        for (TextComponent message : messages) {
+            colored.add(colored(message));
+        }
+        return colored;
+    }
+
+    /**
+     * Add hex color to String
+     * @param message String with hex code
+     * @return Colored String
+     */
+    @NotNull
+    public static Component colored(@Nullable TextComponent message) {
+        if (message == null)
+            return Component.text("");
+
+        String legacy = message.content();
+        return colored(legacy);
+    }
+
+    @NotNull
+    public static Component colored(@Nullable String message) {
+        if (message == null)
+            return Component.text("");
+
+        message = HexColor.convertHexColorToMinecraftHexString(message);
+        Component component = LegacyComponentSerializer.legacy('&').deserialize(message);
+        return component.decoration(TextDecoration.ITALIC, false);
+    }
+
+    @NotNull
+    public static List<Component> uncolored(@Nullable String... messages) {
+        if (messages == null)
+            return Lists.newArrayList();
+
+        List<Component> uncolored = Lists.newArrayListWithCapacity(messages.length);
+        for (String message : messages) {
+            uncolored.add(uncolored(message));
+        }
+        return uncolored;
+    }
+
+    /**
+     * Remove hex color from String[]
+     * @param messages Colored String[]
+     * @return Uncolored String List
+     */
+    @NotNull
+    public static List<Component> uncolored(@Nullable TextComponent... messages) {
+        if (messages == null)
+            return Lists.newArrayList();
+
+        List<Component> uncolored = Lists.newArrayListWithCapacity(messages.length);
+        for (TextComponent message : messages) {
+            uncolored.add(uncolored(message));
+        }
+        return uncolored;
+    }
+
+    @NotNull
+    public static List<Component> uncoloredString(@Nullable Collection<String> messages) {
+        if (messages == null)
+            return Lists.newArrayList();
+
+        List<Component> uncolored = Lists.newArrayListWithCapacity(messages.size());
+        for (String message : messages) {
+            uncolored.add(uncolored(message));
+        }
+        return uncolored;
+    }
+
+    /**
+     * Remove hex color from String Collection
+     * @param messages Colored String Collection
+     * @return Uncolored String List
+     */
+    @NotNull
+    public static List<Component> uncolored(@Nullable Collection<TextComponent> messages) {
+        if (messages == null)
+            return Lists.newArrayList();
+
+        List<Component> uncolored = Lists.newArrayListWithCapacity(messages.size());
+        for (TextComponent message : messages) {
+            uncolored.add(uncolored(message));
+        }
+        return uncolored;
+    }
+
+    @NotNull
+    public static Component uncolored(@Nullable String message) {
+        if (message == null)
+            return Component.text("");
+        return uncolored(Component.text(message));
+    }
+
+    /**
+     * Remove hex color from String
+     * @param message Colored String
+     * @return Uncolored String
+     */
+    @NotNull
+    public static Component uncolored(@Nullable TextComponent message) {
+        if (message == null)
+            return Component.text("");
+        return Component.text(PlainTextComponentSerializer.plainText().serialize(message));
+    }
+
+}
---
+++ applications/lumberlib-app-proxy/src/main/java/me/lede/lumberlibforproxy/color/HexColor.java
@@ -0,0 +1,170 @@
+package me.lede.lumberlibforproxy.color;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public enum HexColor {
+
+    BLACK("#000000"),
+    TULIP_NOIR("#392f31"),
+    MARIE_ROUGE("#edacb1"),
+    ROSE("#8d192b"),
+    MAGENTA("#dc143c"),
+    CORAL("#f29886"),
+    MAROON("#800000"),
+    RED("#ff0000"),
+    ORANGE("#ff7f00"),
+    APRICOT("#fbceb1"),
+    BROWN("#964b00"),
+    MANDARIN("#f89b00"),
+    OCHER("#c68a12"),
+    KHAKI("#8f784b"),
+    YELLOW("#ffd400"),
+    GOLD("#ffd700"),
+    FORSYTHIA("#f7e600"),
+    LIGHT_IVORY("#eee6c4"),
+    IVORY("#ece6cc"),
+    OLIVE("#808000"),
+    BEIGE("#f5f5dc"),
+    GRASS("#6a8518"),
+    LIME("#bfff00"),
+    LIGHT_GREEN("#81c147"),
+    GREEN("#009900"),
+    GREENNESS("#008000"),
+    EMERALD("#008d62"),
+    JADE("#83dcb7"),
+    DARK_BLUE("#008080"),
+    MARINE("#0099a4"),
+    TURQUOISE("#005666"),
+    SKY_BLUE("#50bcdf"),
+    CYAN("#00a3d2"),
+    CYAN_BLUE("#3e91b5"),
+    CERULEAN_FLASH("#0096c6"),
+    LIGHT_BLUE("#4aa8d8"),
+    AQUAMARINE("#5e7e9b"),
+    SHEFFIELD_STEEL("#437299"),
+    BLUE("#0067a3"),
+    PRUSSIAN_BLUE("#003458"),
+    COBALT_BLUE("#00498c"),
+    SEA_BLUE("#0080ff"),
+    ULTRAMARINE("#464964"),
+    WHITE("#ffffff"),
+    GRAY("#808080"),
+    SILVER("#c0c0c0"),
+    LIGHT_PURPLE("#8977ad"),
+    INDIGO("#000080"),
+    BLUE_PURPLE("#6937a1"),
+    PURPLE("#8b00ff"),
+    AMETHYST("#660099"),
+    CLARET("#ff00ff"),
+    PINK("#ff3399")
+    ;
+
+    private static final Pattern PATTERN = Pattern.compile("&#([A-Za-z_]*)#");
+
+    private final String hex;
+
+    HexColor(String hex) {
+        this.hex = hex;
+    }
+
+    /**
+     * 미리 정의된 HexString을 반환합니다.
+     * @return Hex String
+     */
+    public String getHexString() {
+        return hex;
+    }
+
+    /**
+     * 미리 정의된 HexString에 Minecraft Color Code를 부착하여 반환합니다.
+     * @return Minecraft Hex String
+     */
+    public String getMinecraftHexString() {
+        return "&" + hex;
+    }
+
+    /**
+     * 문자열에 포함된 HexColor 를 HexString으로 변환하여 반환합니다. <br>
+     * 입력받은 text가 null이라면 빈 문자열을 반환합니다.<br><br>
+     *
+     * 미리 정의된 HexColor를 입력하여 Text내의 색상 코드를 변환할 수 있습니다. <br>
+     * 정의되지 않은 HexColor를 입력하면 WIHTE 색상으로 적용하여 반환됩니다. <br><br>
+     * 입력 패턴 예시) <br>
+     *  - "&#GREEN#", "&#RED#", "&#PINK#" => O <br>
+     *  - "&#MOL_RU#" => X (정의되지 않은 컬러, WHITE로 변환하여 적용)<br>
+     *  - "&#RED#안녕하신가! &#BLUE#힘세고 &#PINK#강한 아침" => O <br>
+     * @param text String
+     * @return HexColor 가 HexString으로 변환된 문자열
+     */
+    @NotNull
+    public static String convertHexColorToHexString(@Nullable String text) {
+        if (text == null)
+            return "";
+
+        Matcher matcher = PATTERN.matcher(text);
+        StringBuffer buffer = new StringBuffer();
+
+        while (matcher.find()) {
+            String matched = matcher.group(1);
+            HexColor color = getMatchedHexColor(matched);
+            matcher.appendReplacement(buffer, color.getHexString());
+        }
+
+        return matcher.appendTail(buffer).toString();
+    }
+
+    /**
+     * 문자열에 포함된 HexColor를 Minecraft HexString으로 변환하여 반환합니다. <br>
+     * 입력받은 text가 null이라면 빈 문자열을 반환합니다. <br><br>
+     *
+     * 미리 정의된 HexColor를 입력하여 Text내의 색상 코드를 변환할 수 있습니다. <br>
+     * 정의되지 않은 HexColor를 입력하면 WIHTE 색상으로 적용하여 반환됩니다. <br><br>
+     * 입력 패턴 예시) <br>
+     *  - "&#GREEN#", "&#RED#", "&#PINK#" => O <br>
+     *  - "&#MOL_RU#" => X (정의되지 않은 컬러, WHITE로 변환하여 적용)<br>
+     *  - "&#RED#안녕하신가! &#BLUE#힘세고 &#PINK#강한 아침" => O <br>
+     * @param text String
+     * @return HexColor가 Minecraft HexString으로 변환된 문자열
+     */
+    @NotNull
+    public static String convertHexColorToMinecraftHexString(@Nullable String text) {
+        if (text == null)
+            return "";
+
+        Matcher matcher = PATTERN.matcher(text);
+        StringBuffer buffer = new StringBuffer();
+
+        while (matcher.find()) {
+            String matched = matcher.group(1);
+            HexColor color = getMatchedHexColor(matched);
+            matcher.appendReplacement(buffer, color.getMinecraftHexString());
+        }
+
+        return matcher.appendTail(buffer).toString();
+    }
+
+    /**
+     * 문자열에서 HexColor를 추출하여 반환합니다. <br>
+     * 추출할 수 있는 패턴 - "&#([A-Za-z])#" <br>
+     * 문자열에 "&#RED&"가 포함되어 있다면, RED를 추출하여 반환합니다. <br>
+     * 추출할 수 없는 문자를 포함한다면 WHITE 컬러를 반환합니다. <br><br>
+     * ex) <br>
+     *  - "&#GREEN#" => GREEN을 추출하여 반환합니다. <br>
+     *  - "&#MOL_RU#" => 정상적이지 않은 HexColor이므로 WHITE를 반환합니다.
+     * @param matched HexColor가 포함된 문자열
+     * @return HexColor
+     */
+    @NotNull
+    private static HexColor getMatchedHexColor(@NotNull String matched) {
+        matched = matched.replace("&#", "").replace("#", "");
+        try {
+            return HexColor.valueOf(matched.toUpperCase());
+        } catch (Exception ignored) {}
+        return WHITE;
+    }
+
+}
